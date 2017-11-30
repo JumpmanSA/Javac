9d477a98fa86377193616d6e5af5871ae291d6fc
Tshepiso Pholo <Tshepiso Pholo@10.1.64.201>
2017-11-30 14:42:45 +0200

    Hello Guys.

---
+++ Hello_Git/src/application/Hello_World.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Hello_World {
+	
+	public  static void main(String [] args) {
+		
+		System.out.println("You know I'm a stoner stoner stniner");
+		System.out.println("You know I'm a loner loner loner  ");
+	}
+
+}
